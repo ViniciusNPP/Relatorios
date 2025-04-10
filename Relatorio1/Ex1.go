e54f95199adb4e38db39bda1f1975768ca0290eb
Vincius <vnpp856@gmail.com>
2025-04-10 17:00:59 -0300

    Finalizado

--- Relatorio1/Ex1.go
+++ Relatorio1/Ex1.go
@@ -11,7 +11,30 @@ func ParOuImpar(n int) (string) {
 		return "Impar"
 	}
 }
+func SomaAte(n int) (int) {
+	soma := 0
+	for i := 1; i <= n; i++ {
+		soma += i
+	}
+	return soma
+}
+func ClassificarNotas(nota int) string {
+	switch {
+	case nota >= 9 && nota <= 10:
+		return "Muito Bom"
+	case nota >= 7 && nota <= 8:
+		return "Bom"
+	case nota >= 5 && nota <= 6:
+		return "Regular"
+	case nota >= 3 && nota <= 4:
+		return "Ruim"
+	default:
+		return "Muito Ruim"
+	}
+}
 
 func main() {
-	fmt.Println(ParOuImpar(9));
+	fmt.Println(ParOuImpar(9))
+	fmt.Println(SomaAte(6))
+	fmt.Println(ClassificarNotas(8));
 }
